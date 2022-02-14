3acaad3362755a46b7f3d38e55f139dc9a25150b
it21279898 <99582220+it21279898@users.noreply.github.com>
2022-02-14 14:29:54 +0530

    Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,20 @@
 
 #include <stdio.h>
 
-int main() {
+int main() {         
+   
+   float module,sum=0,Avg;
+   int a;
+   
+   for (a=0;a<3;a++)
+   {
+      printf("Input marks %d :",a+1);
+      scanf("%f",&module);
+      sum=sum+module;
+   }
+   
+   Avg=sum/a;
+   printf("the average mark of the student:%.2f",Avg);
   
   return 0;
 }
-
